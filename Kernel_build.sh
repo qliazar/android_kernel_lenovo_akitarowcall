c3cd44e8c2fa6abbdb8f5ee0e9d13883dad29546
qliazar <qliazar001@gmail.com>
2026-03-24 23:52:11 +0300

    source: Update Kernel_build.sh
    Decrease jobs count for stability and make akita_row_call defconfig instead of akita_row_wifi.
    
    Signed-off-by: qliazar <qliazar001@gmail.com>

--- Kernel_build.sh
+++ Kernel_build.sh
@@ -3,5 +3,5 @@ export ARCH=arm64
 export SUBARCH=arm64
 export CROSS_COMPILE=${PWD}/prebuilds/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CC=${PWD}/prebuilds/toolchain/clang-r353983c/bin/clang
-make O=out akita_row_wifi_defconfig
-make  -j128 O=out EXTRA_CFLAGS=-Wno-error EXTRA_CFLAGS=-Wno-error=strict-prototypes 2>&1 | tee Build_log.txt
+make O=out akita_row_call_defconfig
+make  -j4 O=out EXTRA_CFLAGS=-Wno-error EXTRA_CFLAGS=-Wno-error=strict-prototypes 2>&1 | tee Build_log.txt
